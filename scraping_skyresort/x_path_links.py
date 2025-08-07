bae6645a5d648df5611134bbdfc569a28fd44f88
Mueenkhankhattak <khanmueen017@gmail.com>
2025-08-07 07:30:20 -0700

    add_page

--- scraping_skyresort/x_path_links.py
+++ scraping_skyresort/x_path_links.py
@@ -2,28 +2,30 @@ import requests
 from parsel import Selector
 
 total_pages = 32
-
+all_link = []
 #loop through over the pages
 for page in range(1,total_pages + 1):
-    url = f"https://www.skiresort.info/ski-resorts/page/{page}/"
+    if page == 1:
+        url = "https://www.skiresort.info/ski-resorts"
+    else:
+        url = f"https://www.skiresort.info/ski-resorts/page/{page}/"
+        
     response = requests.get(url)
-
     if response.status_code == 200:
         print("Successfully fetched:" ,response)
         sel = Selector(text=response.text)
         
         #Define the path
-        all_link = []
+        
         elements = sel.xpath('//div[@id= "resortList"]//div[@class="h3"]/a')
         for element in elements:
             text = element.xpath("./text()").getall()
             urls = element.xpath("./@href").getall()
-            
-            print(f'Text is: {text} | Url: {urls}')
-            print(len(urls))
+            all_link.append(url)
+            #print(f'Text is: {text} | Url: {urls}')
+            #print(len(urls))
+        print(len(all_link))
             
             
     else:
         print("Error request failed with status code :" , response.status_code)
-
-
