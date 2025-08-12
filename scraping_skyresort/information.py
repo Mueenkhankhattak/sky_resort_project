7a032d177c9476ff41f7784c590e79386b53af88
Mueenkhankhattak <khanmueen017@gmail.com>
2025-08-12 10:42:46 -0700

    add changes

--- scraping_skyresort/information.py
+++ scraping_skyresort/information.py
@@ -8,14 +8,17 @@ if response.status_code == 200:
     sel = Selector(text=response.text)
     
     resort_name = sel.xpath('//span[@class = "fn"]/text()').get()
-    elevation = sel.xpath('//div[@id="selAlti"]/text()').getall()
-    oprerating_time = sel.xpath('//table[@class = "info-table"]//tr/td/text()').getall()
+    elevation = sel.xpath('//div[@id="selAlti"]/text()[2]').get()
+    oprerating_time = sel.xpath('//table[@class = "info-table"]//tr/td/text()').get()
+    resort_website = sel.xpath('//div[@id="selLinkCustomer"]/a/@href').get()
     
-    print(f"Resort name is : {resort_name}")
+    print(f"Resort name is : {(resort_name)}")
     print("***************************")
     print(f"Elevation info is {elevation}")
     print(".************************")
     print(f"operating time is {oprerating_time}")
+    
+    print(f"resort website:  {resort_website}")
     
     contact_page_link = sel.xpath('//a[@class="shaded detail-links link-img"]/@href').get()
     response = requests.get(contact_page_link)
@@ -23,9 +26,15 @@ if response.status_code == 200:
     if response.status_code == 200:
         print(f"Successfult fetched!")
         sel = Selector(text=response.text)
-        contact_information =sel.xpath('//div[@class="col-sm-9"]//p//br//following-sibling::text()').getall()
-        contact_information_website = [text.strip() for text in contact_information if text!=" "]
-        print(f"Contact information is {contact_information_website}")
+        contact_information =sel.xpath('//div[@class="col-sm-9"]/p/text()').get()
+        phone_number = sel.xpath('//div[@class="col-sm-9"]/p/a/@href').get()
+        Email = sel.xpath('//div[@class="col-sm-9"]//p/a[2]/text()').get()
+        reviews = sel.xpath('//*[@id="c50"]/div/div[3]/div[2]/div/div[1]/span/text()').get()
+        
+        print(f"Contact information is {contact_information}")
+        print(f'Phone Number : {phone_number}')
+        print(f"Email is : {Email}")
+        print(f"reviews : {reviews}")
     else:
         print("Error while fetching Html!")
         
--- scraping_skyresort/x_path_links.py
+++ scraping_skyresort/x_path_links.py
@@ -4,7 +4,8 @@ from parsel import Selector
 total_pages = 32
 all_link = []
 #loop through over the pages
-for page in range(1,total_pages + 1):
+page = 1
+while page < total_pages:
     if page == 1:
         url = "https://www.skiresort.info/ski-resorts"
     else:
@@ -25,7 +26,6 @@ for page in range(1,total_pages + 1):
             #print(f'Text is: {text} | Url: {urls}')
             #print(len(urls))
         print(len(all_link))
+    page += 1     
+    
             
-            
-    else:
-        print("Error request failed with status code :" , response.status_code)
