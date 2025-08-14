ef6d66a7c6fadb5faf53f12209f68641c321483e
Mueenkhankhattak <khanmueen017@gmail.com>
2025-08-14 06:58:49 -0700

    path changes

--- scraping_skyresort/information.py
+++ scraping_skyresort/information.py
@@ -8,16 +8,17 @@ if response.status_code == 200:
     sel = Selector(text=response.text)
     
     resort_name = sel.xpath('//span[@class = "fn"]/text()').get()
-    elevation = sel.xpath('//div[@id="selAlti"]/text()[2]').get()
-    oprerating_time = sel.xpath('//table[@class = "info-table"]//tr/td/text()').get()
+    elevation = sel.xpath('//div[@id="selAlti"]/text()').getall()
+    oprerating_time = sel.xpath('//div[@class="description"]//table[@class = "info-table"]//tr//td/text()').getall()
     resort_website = sel.xpath('//div[@id="selLinkCustomer"]/a/@href').get()
-    
-    print(f"Resort name is : {(resort_name)}")
+    reviews = sel.xpath('//span[@class="text-default altitude"]/text()').get()
+
+    #print(f"Resort name is : {resort_name}")
     print("***************************")
-    print(f"Elevation info is {elevation}")
+    print(f"Elevation info is {",".join((elevation))}")
     print(".************************")
-    print(f"operating time is {oprerating_time}")
-    
+    print(f"operating time is {",".join(oprerating_time)}")
+    print(f"reviews = {reviews}")
     print(f"resort website:  {resort_website}")
     
     contact_page_link = sel.xpath('//a[@class="shaded detail-links link-img"]/@href').get()
@@ -26,15 +27,14 @@ if response.status_code == 200:
     if response.status_code == 200:
         print(f"Successfult fetched!")
         sel = Selector(text=response.text)
-        contact_information =sel.xpath('//div[@class="col-sm-9"]/p/text()').get()
-        phone_number = sel.xpath('//div[@class="col-sm-9"]/p/a/@href').get()
+        contact_information =sel.xpath('//div[@class="row"]//div[@class = "col-sm-9"]/p/text()').getall()
+        phone_number = sel.xpath('//div[@class="col-sm-9"]/p/a[@class = "phonenumber"]/text()').get()
         Email = sel.xpath('//div[@class="col-sm-9"]//p/a[2]/text()').get()
-        reviews = sel.xpath('//*[@id="c50"]/div/div[3]/div[2]/div/div[1]/span/text()').get()
         
-        print(f"Contact information is {contact_information}")
+        print(f"Contact information is =  {" ".join(contact_information)}")
         print(f'Phone Number : {phone_number}')
         print(f"Email is : {Email}")
-        print(f"reviews : {reviews}")
+        
     else:
         print("Error while fetching Html!")
         
